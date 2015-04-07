937dbe14cc6eaa006298d35b3954f4dc407c8b8a
Eric Nguyen <neric@berkeley.edu>
2015-04-06 19:53:56 -0700

    Ran scripts on document ready

--- js/main.js
+++ js/main.js
@@ -1,45 +1,52 @@
-var appid = "cf293613-9009-41f2-80e7-56673f77361d";
-var call = null;
+$(document).ready(function() {
+  var appid = "cf293613-9009-41f2-80e7-56673f77361d";
+  var call = null;
 
-// create a client object using the App ID value from Step 2
-var client = new respoke.Client({
-  appId: appid,
-  developmentMode: true
-});
+  var setStatus = function(str) {
+    $("#status").html(str);
+  }
 
-// listen for the 'connect' event
-client.listen('connect', function() {
-    $("#status").html("Connected!");
-});
+  // create a client object using the App ID value from Step 2
+  var client = new respoke.Client({
+    appId: appid,
+    developmentMode: true
+  });
 
-// listen for and answer incoming calls
-client.listen('call', function(evt) {
-     call = evt.call;
-     if (call.initiator !== true) {
-         call.answer({constraints: {audio: true, video: false}});
-         call.listen('hangup', function() {
-            call = null;
-         });
-     }
-});
+  // listen for the 'connect' event
+  client.listen('connect', function() {
+      setStatus("Connected to server!");
+  });
 
-// now connect when the user clicks the 'Connect' button
-$("#doLogin").click(function() {
-    var endpoint =  $("#endpoint").val();
-    client.connect({
-         endpointId: endpoint
-    });
-});
+  // listen for and answer incoming calls
+  client.listen('call', function(evt) {
+       call = evt.call;
+       if (call.initiator !== true) {
+           call.answer({constraints: {audio: true, video: false}});
+           call.listen('hangup', function() {
+              call = null;
+           });
+       }
+  });
 
-// Create a call, hangup on click again
-$("#makeCall").click(function() {
-    if (call) {
-      call.hangup();
-      call = null;
-      $("#makeCall").val("Connect");
-    } else {
-      var endpoint = client.getEndpoint({"id" : $("#remoteId").val()});
-      call = endpoint.startAudioCall();
-      $("#makeCall").val("Disconnect");
-    }
+  // now connect when the user clicks the 'Connect' button
+  $("#doLogin").click(function() {
+      var endpoint =  $("#endpoint").val();
+      setStatus("Connecting to servers...");
+      client.connect({
+           endpointId: endpoint
+      });
+  });
+
+  // Create a call, hangup on click again
+  $("#makeCall").click(function() {
+      if (call) {
+        call.hangup();
+        call = null;
+        $("#makeCall").val("Connect");
+      } else {
+        var endpoint = client.getEndpoint({"id" : $("#remoteId").val()});
+        call = endpoint.startAudioCall();
+        $("#makeCall").val("Disconnect");
+      }
+  });
 });
